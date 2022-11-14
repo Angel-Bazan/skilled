520517fa11637f969f1d686b0c92cd5a0f9a8e97
Angel <angelsanchez.as524@gmail.com>
2022-11-14 15:49:19 -0800

    unfavorite functionality

--- client/src/components/favorite.js
+++ client/src/components/favorite.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-// import Trade from "./trades";
+import Trade from "./trade";
 
 const Favorite = () => {
   const [trades, SetTrades] = useState([]);
@@ -20,37 +20,8 @@ const Favorite = () => {
         <h2>Favorite Trades List</h2>
       </div>
       {/* {favorites.join(":")} */}
-      <ul id="favorite" className="row justify-content-md-center list-unstyled">
-        {trades.map((trade) => (
-          <li key={trade.id} className="col col-sm-4 mb-3">
-            <div className="card" style={{ width: "18rem" }}>
-              <img src={trade.img} className="card-img-top" alt="..."></img>
-              <div className="card-body">
-                <br />
-                <strong>Name:</strong>
-                {trade.name}
-                <br />
-                <strong>Link:</strong>
-                {trade.link} <br />
-                <strong>Content:</strong>
-                {trade.content}
-                <br />
-                <strong>Colleges:</strong>
-                <br />
-                <button>
-                  <span
-                    className="material-symbols-outlined"
-                    // onClick={() => }
-                  >
-                    unfavorite
-                  </span>
-                </button>
-                <br />
-              </div>
-            </div>
-          </li>
-        ))}
-      </ul>
+     
+      <Trade trades={trades} isFavorite={false} onUpdate={getFavorites}/>
     </div>
   );
 };
