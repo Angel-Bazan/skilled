4c1ca9ed292e13df135b87e1f5113be440c8f9a5
Angel <angelsanchez.as524@gmail.com>
2022-11-14 12:36:00 -0800

    merging

--- client/src/components/auth_files/logout-button.js
+++ client/src/components/auth_files/logout-button.js
@@ -9,30 +9,14 @@ const LogoutButton = () => {
   const [activeUser, setActiveUser] = useState([]);
 
   const getUser = async () => {
-<<<<<<< HEAD
-    // const response = await fetch(`http://localhost:5000/api/user`);
-    // const currentUser = await response.json();
-=======
-    // const response = await fetch(`/api/user`);
-    // const user = await response.json();
->>>>>>> skilled
     setActiveUser(user);
   };
 
   useEffect(() => {
     getUser();
     console.log(activeUser, "users");
-<<<<<<< HEAD
-  }, [user]);
 
-  return (
-    <div className="logout">
-      <div className="username">
-      Hello,{activeUser && activeUser.given_name}
-      </div>
-      <div>
-=======
-  }, []);
+  }, [user]);
 
   return (
     <div className="logout">
@@ -41,7 +25,7 @@ const LogoutButton = () => {
         <h5>Hello {activeUser && activeUser.given_name}</h5>
       </div>
       <div className="logoutB">
->>>>>>> skilled
+
       <button
         className="btn btn-danger btn-block"
         onClick={() =>
